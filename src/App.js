b1b0e59d4eeb920f3beb57f94849d8cc9c5eadd6
mohamad <mohamad@gmail.com>
2023-03-25 15:07:07 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './App.css'
 import Sidebar from './Components/Sidebar/Sidebar'
+import Header from './Components/Header/Header'
+
+
 export default function App() {
   return (
-    <div>
-      <Sidebar />
+    <div className='main'>
+        <Sidebar />
+      
+      <div className='main-right'>
+        <Header />
+      </div>
     </div>
   )
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Header.css'
+import {BsFillBrightnessHighFill} from 'react-icons/bs'
+import {BsBell} from 'react-icons/bs'
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <div className='header-right'>
+            <img src="./soran.JPG" alt="" />
+            <div className='header-right-text'>
+                <h1>سوران غفوری</h1>
+                <p>برنامه نویس فرانت اند</p>
+            </div>
+        </div>
+        <div className='header-left'>
+            <div className='search-box'>
+                <input type="text" placeholder='جست و جو کنید...'/>
+                <button>جست و جو</button>
+            </div>
+            <button><BsFillBrightnessHighFill/></button>
+            <button><BsBell/></button>
+            
+        </div>
+    </div>
+  )
+}
