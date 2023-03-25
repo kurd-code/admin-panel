743e6341221a927a5440091b12dd09b477912e3a
soran <soransorani1955@gmail.com>
2023-03-25 16:27:22 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from 'react'
 import './App.css'
 import Sidebar from './Components/Sidebar/Sidebar'
 import Header from './Components/Header/Header'
-
+import { Route, Routes, useRoutes } from 'react-router-dom'
+import routes from './routes'
 
 export default function App() {
+
+  const router = useRoutes(routes)
+
   return (
     <div className='main'>
         <Sidebar />
       
       <div className='main-right'>
         <Header />
+
+        {router}
+
       </div>
     </div>
   )
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,34 +1,54 @@
 import React from 'react'
 import './Sidebar.css'
+import { Link } from 'react-router-dom'
 import {AiOutlineHome} from 'react-icons/ai'
 import {MdProductionQuantityLimits} from 'react-icons/md'
 import {BiCommentDetail} from 'react-icons/bi'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiShoppingBag} from 'react-icons/bi'
 import {BiDollar} from 'react-icons/bi'
+
 export default function Sidebar() {
   return (
     <div className='sidebar'>
         <h1 className="sidebar-title">به داشبورد خود خوش امدید</h1>
 
         <ul className='sidebar-links'>
+            
             <li>
-                <a href="#" ><AiOutlineHome className='icon'/>  صفحه ی اصلی </a>
+               <a href="#"><AiOutlineHome className='icon'/>  صفحه ی اصلی </a>
             </li>
+            
+            
             <li className='active'>
-                <a href="#" > <MdProductionQuantityLimits className='icon'/> محصولات</a>
+                <Link to='/products'>
+                <MdProductionQuantityLimits className='icon'/> محصولات
+                </Link>
+                
             </li>
             <li>
-                <a href="#"> <BiCommentDetail className='icon'/> کامنت ها</a>
+                <Link to='/comments'>
+                <BiCommentDetail className='icon'/> کامنت ها
+                </Link>
+                
             </li>
             <li>
-                <a href="#"> <AiOutlineUser className='icon'/> کاربران</a>
+                <Link to='/users'>
+                <AiOutlineUser className='icon'/> کاربران
+                </Link>
+                
             </li>
             <li>
-                <a href="#"> <BiShoppingBag className='icon'/> سفارشات</a>
+                <Link to='/orders'>
+                <BiShoppingBag className='icon'/> سفارشات
+                </Link>
+                
             </li>
             <li>
-                <a href="#"> <BiDollar className='icon'/> تخفیف ها</a>
+                <Link to='/offs'>
+                <BiDollar className='icon'/> تخفیف ها
+                </Link>
+                
             </li>
         </ul>
     </div>
